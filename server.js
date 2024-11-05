9edbdc4c60851d8195216a37fc0308931709bc33
Kaiosuke <trongleele@gmail.com>
2024-11-05 08:54:15 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const jsonServer = require("json-server");
+const cors = require("cors");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+const corsOptions = {
+  origin: "http://localhost:3000", // Chỉ cho phép truy cập từ origin này
+};
+
+server.use(cors(corsOptions));
+server.use(middlewares);
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/blog/:resource/:id/show": "/:resource/:id",
+  })
+);
+server.use(router);
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
+
+module.exports = server;
